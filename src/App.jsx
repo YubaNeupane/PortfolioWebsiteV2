8b93abca889b0c7052c68cd8af688b09b86a20d8
Yuba Neupane <yjn5031@psu.edu>
2023-03-20 21:18:44 -0400

    deplot v-0.1

--- src/App.jsx
+++ src/App.jsx
@@ -11,41 +11,12 @@ import {
   StarsCanvas,
 } from "./components";
 import Footer from "./components/Footer";
-import ParticleBackground from "react-particle-backgrounds";
 
 const App = () => {
-  const settings = {
-    canvas: {
-      canvasFillSpace: true,
-      width: window.innerWidth,
-      height: window.innerHeight,
-      useBouncyWalls: true,
-    },
-    particle: {
-      particleCount: 100,
-      color: "#94ecbe",
-      minSize: 2,
-      maxSize: 5,
-    },
-    velocity: {
-      directionAngle: 0,
-      directionAngleVariance: 360,
-      minSpeed: 0.1,
-      maxSpeed: 1,
-    },
-    opacity: {
-      minOpacity: 0,
-      maxOpacity: 0.5,
-      opacityTransitionTime: 3000,
-    },
-  };
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary scroll-smooth scrollbar-hide">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <div className="absolute">
-            <ParticleBackground settings={settings} />
-          </div>
           <Navbar />
           <Hero />
         </div>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "./",
+  plugins: [viteCommonjs(), react()],
+  build: {
+    commonjsOptions: {
+      defaultIsModuleExports(id) {
+        try {
+          const module = require(id);
+          if (module?.default) {
+            return false;
+          }
+          return "auto";
+        } catch (error) {
+          return "auto";
+        }
+      },
+      transformMixedEsModules: true,
+    },
+  },
 });
